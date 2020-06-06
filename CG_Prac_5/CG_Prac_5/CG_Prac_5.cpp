14c75104ffdf5c9ee60e863799c4195ac66216e7
김기범 <38284922+gibum1228@users.noreply.github.com>
2020-06-06 12:12:27 +0900

    평행투상 원근투상

--- CG_Prac_5/CG_Prac_5/CG_Prac_5.cpp
+++ CG_Prac_5/CG_Prac_5/CG_Prac_5.cpp
@@ -2,69 +2,42 @@
 #include<GL/gl.h>
 #include<GL/glu.h>
 
-static int day = 0, time = 0;
-GLboolean isParallel = true;
+static int Day = 0, Time = 0;
+static GLboolean isOrtho = true;
 
-void myDisplay() {
-
+void MyDisplay(void)
+{
 	glClear(GL_COLOR_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glColor3f(1.0, 0.3, 0.3);
-	glutWireSphere(0.2, 20, 16); // 태양 그리기
-	glPushMatrix(); // 현재의 모델 좌표계를 스택에 푸시하여 저장
-		glRotatef((GLfloat)day, 0.0, 1.0, 0.0); // 현 모델 좌표계를 y축을 기준으로 각도 day만큼 회전시킨 후 0.7만큼 오른쪽으로 이동시키는 과정 (4도 동일) !3, 4 순서 바뀌면 안 됨
-		glTranslatef(0.7, 0.0, 0.0); // 4
-		glRotatef((GLfloat)time, 0.0, 5.0, 0.0); // 각도 time만큼 회전 ( 지구 자전을 위한 것)
-		glColor3f(0.5, 0.6, 0.7);
-		glutWireSphere(0.1, 10, 8); // 지구의 모델 좌표계 Me에 지구를 그리기
-		glPushMatrix(); // 지구 모델 좌표계를 저장
-			glRotatef((GLfloat)time, 0.0, 5.0, 0.0); // 현 지구 모델 좌표계를 회전시키되 y축을 기준으로 각도 time만큼 회전
-			glTranslatef(0.2, 0.0, 0.0); // 이 결과로 달의 모델 좌표계 Mm 생성
-			glColor3f(0.9, 0.9, 0.2);
-			glutWireSphere(0.04, 10, 8); // 달을 그려내기
-		glPopMatrix();
+
+	glColor3f(1.0f, 0.3f, 0.3f);
+	glutWireSphere(0.2, 20, 16);						// 1
+	glPushMatrix();										// 2 - 모델뷰 행렬 저장(태양 위치)
+	glRotatef((GLfloat)Day, 0.0f, 1.0f, 0.0f);		// 3
+	glTranslatef(0.7f, 0.0f, 0.0f);					// 4
+	glRotatef((GLfloat)Time, 0.0f, 1.0f, 0.0f);		// 5
+	glColor3f(0.5f, 0.6f, 0.7f);
+	glutWireSphere(0.1f, 10, 8);					// 6
+	glPushMatrix();									// 7 - 모델뷰 행렬 저장(지구 위치)
+	glRotatef((GLfloat)Time, 0.0f, 1.0f, 0.0f);	// 8
+	glTranslatef(0.2f, 0.0f, 0.0f);				// 9
+	glColor3f(0.9f, 0.8f, 0.2f);
+	glutWireSphere(0.04f, 10, 8);				// 10
+	glPopMatrix();
 	glPopMatrix();
 
-	if (!isParallel) {
-		glOrtho(-1.0, 1.0, -1.0, 1.0, 1.0, 10.0);
-
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-	}
-	else {
-		glFrustum(-5.0, 5.0, -5.0, 5.0, 200.0, 1000.0);
-
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-	}
-
 	glutSwapBuffers();
-	glFlush();
 }
 
-void myMainMenu(int entryID) {
-	if (entryID == 1) {
-		isParallel = true;
-	}
-	else if (entryID == 2) {
-		isParallel = false;
-	}
-	else if (entryID == 3) {
-		exit(0);
-	}
-
-	glutPostRedisplay();
-}
-
-void myKeyboard(unsigned char key, int x, int y) {
-	switch (key) {
-	case 'd': // d를 누를 때마다 공전
-		day = (day + 10) % 360;
+void MyKeyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'd':
+		Day = (Day + 10) % 360;
 		glutPostRedisplay();
 		break;
-	case 't': // t를 누를 때마다 자전
-		time = (time + 5) % 360;
+	case 't':
+		Time = (Time + 5) % 360;
 		glutPostRedisplay();
 		break;
 	default:
@@ -72,26 +45,69 @@ void myKeyboard(unsigned char key, int x, int y) {
 	}
 }
 
-int main(int argc, char** argv) {
+void Projection(void)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (isOrtho)
+	{
+		glOrtho(-2.0, 2.0, -2.0, 2.0, -3.0, 3.0);
+	}
+	else
+	{
+		gluPerspective(120.0, 1.0, 0.5, 10);
+	}
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+
+void MyMenu(int entryID)
+{
+	switch (entryID)
+	{
+	case 1:
+		isOrtho = true;
+		Projection();
+		glutPostRedisplay();
+		break;
+	case 2:
+		isOrtho = false;
+		Projection();
+		glutPostRedisplay();
+		break;
+	default:
+		exit(1);
+		break;
+	}
+}
+
+void MyReshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	Projection();
+}
+
+int main(int argc, char** argv)
+{
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("OpenGL Sample Drawing");
+
 	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glViewport(0, 0, 500, 400);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	
-	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-	GLint myMainMenuID = glutCreateMenu(myMainMenu);
-	glutAddMenuEntry("Parallel", 1);
-	glutAddMenuEntry("Perspective", 2);
+
+	GLint MyMenuID = glutCreateMenu(MyMenu);
+	glutAddMenuEntry("Parallel Projection", 1);
+	glutAddMenuEntry("Perspective Projection", 2);
 	glutAddMenuEntry("Exit", 3);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
-	glutDisplayFunc(myDisplay);
-	glutKeyboardFunc(myKeyboard);
+	glutDisplayFunc(MyDisplay);
+	glutKeyboardFunc(MyKeyboard);
+	glutReshapeFunc(MyReshape);
 	glutMainLoop();
 
 	return 0;
